dd7e15db115b3af7d4f684f35281182945321805
Phuoc Pham <phamhbphuoc@gmail.com>
2025-10-26 15:18:49 +0900

    move out from /docs

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -1,8 +1,9 @@
-import { source } from "@/lib/source"
-import { DocsLayout } from "fumadocs-ui/layouts/docs"
+import type { ReactNode } from "react"
 import { baseOptions } from "@/app/layout.config"
+import { DocsLayout } from "fumadocs-ui/layouts/docs"
+import { source } from "@/lib/source"
 
-export default function Layout({ children }: LayoutProps<"/docs">) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout
       tree={source.pageTree}
